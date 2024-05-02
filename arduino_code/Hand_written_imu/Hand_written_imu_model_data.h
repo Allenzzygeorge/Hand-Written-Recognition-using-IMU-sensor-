60eb62a96b3af3989e54cc9f174e57c295041aa4
Allenzzygeorge <148372527+Allenzzygeorge@users.noreply.github.com>
2024-05-02 19:22:43 +0530

    Arduino code for running in nano 33 ble.
    Download entire folder and open arduio IDE then use open from files to open the code

---
+++ arduino_code/Hand_written_imu/Hand_written_imu_model_data.h
@@ -0,0 +1,10 @@
+
+// xxd -i magic_wand_model.tflite > magic_wand_model_data.cc
+
+#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_MAGIC_WAND_MAGIC_WAND_MODEL_DATA_H_
+#define TENSORFLOW_LITE_MICRO_EXAMPLES_MAGIC_WAND_MAGIC_WAND_MODEL_DATA_H_
+
+extern const unsigned char g_magic_wand_model_data[];
+extern const int g_magic_wand_model_data_len;
+
+#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_MAGIC_WAND_MAGIC_WAND_MODEL_DATA_H_
